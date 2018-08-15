daf74546d5f0674f40f6cef8ba81641d5b1acd1b
guoqing16 <guoqing16@staff.sina.com.cn>
2018-08-15 22:29:07 +0800

    轮播图

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -2,10 +2,101 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Http\Requests\Backend\BannerRequest;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class BannerController extends Controller
 {
-    //
+    /**
+     * 说明: 缩略图管理列表页
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author 郭庆
+     */
+    public function index(Request $request)
+    {
+        $result = Banner::orderBy('updated_at', 'desc')
+            ->orderBy('sort', 'desc')
+            ->paginate(15);
+        return view('backend.banner.index', ['banners' => $result]);
+    }
+
+    /**
+     * 说明: 创建缩略图视图
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author 郭庆
+     */
+    public function create()
+    {
+        return view('backend.banner.create');
+    }
+
+    /**
+     * 说明: 添加缩略图
+     *
+     * @param BannerRequest $request
+     * @return mixed
+     * @author 郭庆
+     */
+    public function store(BannerRequest $request)
+    {
+        Banner::create([
+            'title' => $request->title,
+            'file' => $request->file,
+            'url' => $request->url,
+            'sort' => $request->sort,
+        ]);
+
+        return redirect()->route('admin.banner.index')->withFlashSuccess('添加缩略图成功');
+    }
+
+    /**
+     * 说明: 修改视图
+     *
+     * @param Banner $banner
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author 郭庆
+     */
+    public function edit(Banner $banner)
+    {
+        return view('backend.banner.edit', ['banner' => $banner]);
+    }
+
+    /**
+     * 说明: 修改缩略图
+     *
+     * @param Banner $banner
+     * @param BannerRequest $request
+     * @return mixed
+     * @author 郭庆
+     */
+    public function update(Banner $banner, BannerRequest $request)
+    {
+        Banner::where(['id' => $banner->id])->update([
+            'title' => $request->title,
+            'file' => $request->file,
+            'url' => $request->url,
+            'sort' => $request->sort,
+        ]);
+
+        return redirect()->route('admin.banners.index')->withFlashSuccess('修改缩略图成功');
+    }
+
+    /**
+     * 说明: 修改缩略图
+     *
+     * @param Banner $banner
+     * @return mixed
+     * @author 郭庆
+     */
+    public function destroy(Banner $banner)
+    {
+        $banner->delete();
+
+        return redirect()->route('admin.banners.index')->withFlashSuccess('删除缩略图成功');
+    }
 }
--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -13,4 +13,37 @@ class Banner extends Model
     protected $guarded = [];
 
     protected $dates = ['deleted_at'];
+
+    public function getEditButtonAttribute()
+    {
+        return '<a href="' . route('admin.banners.edit', $this) . '" class="btn btn-primary"><i class="fas fa-edit" data-toggle="tooltip" data-placement="top" title="' . __('buttons.general.crud.edit') . '"></i></a>';
+    }
+
+    public function getDeleteButtonAttribute()
+    {
+        return '<a href="' . route('admin.banners.destroy', $this) . '"
+                 data-method="delete"
+                 data-trans-button-cancel="' . __('buttons.general.cancel') . '"
+                 data-trans-button-confirm="' . __('buttons.general.crud.delete') . '"
+                 data-trans-title="' . __('strings.backend.general.are_you_sure') . '"
+                 class="dropdown-item">' . __('buttons.general.crud.delete') . '</a> ';
+    }
+
+
+    public function getActionButtonsAttribute()
+    {
+        return '
+    	<div class="btn-group btn-group-sm" role="group" aria-label="User Actions">
+		  ' . $this->edit_button . '
+		
+		  <div class="btn-group" role="group">
+			<button id="userActions" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  More
+			</button>
+			<div class="dropdown-menu" aria-labelledby="userActions">
+			  ' . $this->delete_button . '
+			</div>
+		  </div>
+		</div>';
+    }
 }
