fd8500307d8f8ba70dd306637979f1c4ba0ee74e
lindowx <lindowx@gmail.com>
2018-08-28 14:45:11 +0800

    修改后台文本编辑器配置；修改前端文章页的视频播放器样式

--- resources/views/backend/article/create.blade.php
+++ resources/views/backend/article/create.blade.php
@@ -207,12 +207,7 @@
 @endsection
 @section('scripts')
     <script type="text/javascript">
-        var ue = UE.getEditor('content', {
-            autoHeightEnabled: false,
-            minFrameHeight: 500,
-            scaleEnabled: true,
-            autoFloatEnabled: true,
-        });
+        var ue = UE.getEditor('content', {});
         ue.ready(function () {
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
