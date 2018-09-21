3bad8f927c7644ea5a6578e0338934e6c7586893
lindowx <lindowx@gmail.com>
2018-09-21 15:24:08 +0800

    文章表新增bot字段用来区分人工编辑和机器人抓取数据

---
+++ database/migrations/2018_09_21_071634_update_article_atlas_tables.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateArticleAtlasTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->unsignedTinyInteger('bot')
+                ->default(0)
+                ->comment('爬虫机器人id；0=人工编辑； 大于0=爬虫抓取');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('bot');
+        });
+    }
+}
